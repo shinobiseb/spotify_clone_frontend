0b69a457a888721a7fa87f0ff5314b8cbeecd723
shinobiseb <sebpatin@gmail.com>
2023-07-22 12:28:19 -0400

    Your library section completed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,24 +6,54 @@ import LibraryMusicRoundedIcon from '@mui/icons-material/LibraryMusicRounded';
 
 export default function Nav() {
   return (
-    <>
-        <nav className='block w-64'>
-                <ul className='nav-div bg-bg-black text-lg block py-2 px-3 rounded-lg'>
-                    <li className='nav-li'>
-                        <a className='nav-link' href="#Home"> 
-                            <HomeRoundedIcon style={{ color: '#a5a5a5'}}/>
-                            <span>Home</span>
-                        </a>
-                    </li>
-                    <li className='nav-li'>
-                        <a className='nav-link' href="#search"> 
-                            <SearchRoundedIcon style={{ color: '#a5a5a5'}}/>
-                            <span>Search</span>
-                        </a>
-                    </li>  
-                </ul>
+    <div className='nav block w-navSize'>
+        {/* ----------- Header -----------*/}
+        <ul className='nav-div bg-bg-black text-lg block py-2 px-3 rounded-lg'>
+            <li className='nav-li'>
+                <a className='nav-item flex' href="#Home"> 
+                    <HomeRoundedIcon className='icon' style={{ 
+                        color: '#a5a5a5', 
+                        fontSize: '2rem'
+                        }}/>
+                    <span>Home</span>
+                </a>
+            </li>
+            <li className='nav-li'>
+                <a className='nav-item flex' href="#search"> 
+                    <SearchRoundedIcon className='icon' style={{ 
+                        color: '#a5a5a5', 
+                        fontSize: '2rem'
+                        }}/>
+                    <span className='pl-4'>Search</span>
+                </a>
+            </li>  
+        </ul>
+        {/* ----------- End of Header -----------*/}
 
-        </nav>
-    </>
+        {/* ----------- Library -----------*/}
+        <div className='library nav-div bg-bg-black text-lg py-2 px-3 rounded-lg mt-2 block'>
+            <div className='w-full flex justify-between items-center'>
+                <button className='nav-item flex-row'> 
+                    <LibraryMusicRoundedIcon className='icon' 
+                        style={{ 
+                        color: '#a5a5a5', 
+                        fontSize: '2rem'
+                        }}/>
+                    <span className='pl-3'> Your Library </span>
+                </button>
+                    <AddRoundedIcon className='icon' 
+                        style={{ 
+                        color: '#a5a5a5', 
+                        fontSize: '2rem'
+                        }}/>
+            </div>
+            <section className='bg-nav-bg-grey rounded-lg text-white px-5 py-4 justify-items-start w-full flex flex-col justify-between my-2 flex-start min-h-full h-36'>
+                    <span className=' text-md font-bold'>Create your first playlist</span>
+                    <span className='block text-sm'>It's easy, we'll help you</span>
+                <button className='bg-white text-black rounded-2xl font-bold px-3 py-1 w-40 mt-4'>
+                    Create Playlist</button>
+            </section>
+        </div>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,23 @@ export default {
       'nav-bg-grey': '#242424',
       'div-bg-grey': '#151515',
       'text-grey': '#a2a2a2',
-      'unhovered-grey': '#a5a5a5'
+      'unhovered-grey': '#a5a5a5',
+      'white': '#ffffff',
+      'black': '#000000',
     },
-    extend: {},
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      'smd': '1.3',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {width: {
+      'navSize': '280px',
+    }},
   },
   plugins: [],
 }
