c9d58dd609bc1ee900ae10cc0f9e4898eb1e5826
shinobiseb <sebpatin@gmail.com>
2023-07-23 15:33:52 -0400

    header buttons disappear at md

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
 
 
   return (
-    <div className='flex flex-row justify-between items-center h-16 w-full z-100'>
+    <div className='flex flex-row justify-between items-center h-16 z-100'>
         <button className='ml-5 bg-black px-2 py-1 rounded-full'>
             <ArrowBackIosNewRoundedIcon className='icon' 
                         style={{ 
@@ -14,9 +14,9 @@ export default function Header() {
         </button>
 
         <div className='flex w-3/4 max-w-lg justify-around items-center'>
-            <button className='search-link'>Premium</button>
-            <button className='search-link'>Support</button>
-            <button className='search-link'>Download</button>
+            <button className='search-link hidden md:inline-block'>Premium</button>
+            <button className='search-link hidden md:inline-block'>Support</button>
+            <button className='search-link hidden md:inline-block'>Download</button>
             <div className='bar'></div>
             <button className='search-link'>Sign up</button>
             <button className='white-button'>Log in</button>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import Header from './Header'
 import PlaylistGallery from './PlaylistGallery'
+
 
 export default function Main() {
   return (
